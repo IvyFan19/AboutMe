bbe902da05db55556b2abb6920589277cec73226
Xinwei Fan <luluvision.ivy@gmail.com>
2024-07-06 12:32:47 -0700

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Ivy Fan on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
